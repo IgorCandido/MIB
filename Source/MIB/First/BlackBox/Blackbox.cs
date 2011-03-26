508ebf1ba07bde50caa404a649728ff76e662820
Igor Candido <igr.cndd@gmail.com>
2011-03-26 12:08:21 +0000

    First Final Design

--- Source/MIB/First/BlackBox/Blackbox.cs
+++ Source/MIB/First/BlackBox/Blackbox.cs
@@ -2,32 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using First.Interfaces;
+using First_Interfaces;
+using First_Interfaces.ClientNodeInformationTypes;
+using First_Interfaces.Contracts;
+using First_Interfaces.ServiceInterfaces;
 
 namespace First.BlackBox
 {
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Blackbox : IBlackbox
     {
 
         Hashtable subscriptionsTable = new Hashtable(); 
         Hashtable emittersTable = new Hashtable();
 
+        public Blackbox()
+        {
+
+            emittersTable["TCP"] = ProxyFactory.GetProxy<IEmitter>("tcpEmitter");
+
+        }
+
         #region Implementation of IBlackbox
 
         public void Receive(string topic, object data)
         {
-            List<IClientNode> subscriptions;
+            List<ClientInformationContract> subscriptions;
 
-            if ((subscriptions = (subscriptionsTable[topic]) as List<IClientNode>) != null)
+            
+            if ((subscriptions = (subscriptionsTable[topic]) as List<ClientInformationContract>) != null)
             {
 
                 IEmitter emitter;
 
-                foreach (IClientNode clientInformation in subscriptions)
+                foreach (ClientInformationContract clientInformation in subscriptions)
                 {
                     
-                     if( ( emitter = emittersTable[clientInformation.emitterType] as IEmitter ) != null )
+                     if( ( emitter = emittersTable[clientInformation.EmitterType] as IEmitter ) != null )
                      {
                          
                          emitter.Emit(clientInformation, data);
@@ -40,20 +54,24 @@ namespace First.BlackBox
             }
         }
 
-        public void Subscribe(string topic, IClientNode clientInformation)
+        public void Subscribe(string topic, ClientInformationContract clientInformation)
         {
-            
-            List<IClientNode> subscriptions;
 
-            if ((subscriptions = (subscriptionsTable[topic]) as List<IClientNode>) != null)
+            List<ClientInformationContract> subscriptions;
+
+            if ((subscriptions = (subscriptionsTable[topic]) as List<ClientInformationContract>) == null)
             {
 
-                if (!subscriptions.Contains(clientInformation))
-                {
+                subscriptions = new List<ClientInformationContract>();
 
-                    subscriptions.Add(clientInformation);
+                subscriptionsTable[topic] = subscriptions;
 
-                }
+            }
+
+            if (!subscriptions.Contains(clientInformation))
+            {
+
+                subscriptions.Add(clientInformation);
 
             }
         }
